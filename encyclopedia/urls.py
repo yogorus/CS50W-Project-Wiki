43a6659e427cb8072edae5d0b6ebc3cb385cafc8
yogorus <yogorus@gmail.com>
2023-01-15 20:29:17 +0300

    Actually added entry creation

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('wiki/<str:entry>', views.wiki, name='wiki'),
-    path('search', views.search, name='search')
+    path('search', views.search, name='search'),
+    path('create', views.create, name="create")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django import forms
 import markdown2
 
 from . import util
 
+class CreateEntryForm(forms.Form):
+    title = forms.CharField(label="Title")
+
+    text = forms.CharField(widget=forms.Textarea, label='Text')
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -39,4 +44,26 @@ def search(request):
     # Render items
     return render(request, 'encyclopedia/search.html', {
         'entries': entries
+    })
+
+"""Create new wiki entry"""
+def create(request):
+    if request.method == "POST":
+        form = CreateEntryForm(request.POST)
+        
+        if form.is_valid():
+            title = form.cleaned_data['title'].strip()
+            text = form.cleaned_data['text'].strip()
+            
+            # Check if similar entry exists
+            if util.get_entry(title):
+                return HttpResponse('<h1>403 Such entry already exists</h1>', status=403)
+            
+            # Write new entry file into entries directory
+            util.save_entry(title, bytes(text, 'utf8'))
+            # with open(f'./entries/{title}.md', 'w+') as file:
+            #     file.write(text.strip())
+
+    return render(request, "encyclopedia/create.html", {
+        "form" : CreateEntryForm()
     })
